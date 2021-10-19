10199d07e607f448bc7658e46eff06297125c52a
zhenee <zheneewong@gmail.com>
2021-10-19 12:45:40 +0800

    first commit

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,17 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.post("/subscribe", function(req, res, next) {
+  const { name, email } = req.body;
+
+  // 1. Validate the user data
+  // 2. Subscribe the user to the mailing list
+  // 3. Send a confirmation email
+
+  res.render("subscribed", {
+    title: "You are subscribed",
+    name,
+    email
+  });
+});
